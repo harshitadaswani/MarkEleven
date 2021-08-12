e6825fcb09f1bdb0aaa9e8d6ba676e00ee5e3c92
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-13 03:36:16 +0530

    js done

--- src/app.js
+++ src/app.js
@@ -3,22 +3,35 @@ const luckyNumber = document.querySelector("#luckyNo");
 const checkButton = document.querySelector("#check-Btn");
 const result = document.querySelector("#output");
 
+function divisiblity(sum, luckyNumber) {
+  //console.log(sum, luckyNumber);
+  if (sum % luckyNumber === 0) {
+    result.innerText = "Yay!!! Your Birthday is lucky.";
+  } else {
+    result.innerText = "Nay!!! Your birthday isn't that lucky.";
+  }
+}
+
 function clickHandlerCheck() {
   const Bdate = birthDate.Value;
   console.log(Bdate);
   const sum = calculateSum(Bdate);
   console.log(sum);
+  if (sum && Bdate) {
+    divisiblity(sum, luckyNumber.Value);
+  } else {
+    result.innerText = "Enter values to continue";
+  }
 }
 
 function calculateSum(Bdate) {
-  var add = 0;
+  Bdate = Bdate.replaceAll("-", "");
+  var sum = 0;
   var i;
   for (i = 0; i < Bdate.length; i++) {
-    if (Bdate[i] !== "-") {
-      add = add + Number(Bday.charAt(i));
-      return add;
-    }
+    sum = sum + Number(Bdate.charAt(i));
   }
+  return sum;
 }
 
 checkButton.addEventListener("click", clickHandlerCheck);
